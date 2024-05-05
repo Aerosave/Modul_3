34c591c27dcfefb59fb33ac1625c88feb4563d1b
Nikita Anisimov <nikitanisimov98@gmail.com>
2024-05-05 11:58:03 +1000

    add task with data

---
+++ Modul_3/My_Name.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        const string MyName = "Никита"; 
+        byte age = 25;
+        byte shoesize = 44;
+        bool pet = true;
+        Console.WriteLine("My nane is "+MyName);
+        Console.WriteLine("My age is " + age);
+        Console.WriteLine("Do I hane a pet? " + pet);
+        Console.WriteLine("Me shoe size is " + shoesize); 
+        Console.ReadKey();  
+    }
+    
+    
+      
+}
