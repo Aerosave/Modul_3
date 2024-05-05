5f031bfc4dbdc046e60d6b86a7a1ff9dfbd0730b
Nikita Anisimov <nikitanisimov98@gmail.com>
2024-05-05 12:13:40 +1000

    red

--- Modul_3/My_Name.cs
+++ Modul_3/My_Name.cs
@@ -1,20 +1,18 @@
 ﻿using System;
-
 class MainClass
 {
     public static void Main(string[] args)
     {
-        const string MyName = "Никита"; 
+        const string MyName = "Никита";
         byte age = 25;
         byte shoesize = 44;
         bool pet = true;
-        Console.WriteLine("My nane is "+MyName);
+        Console.WriteLine("My nane is " + MyName);
         Console.WriteLine("My age is " + age);
         Console.WriteLine("Do I hane a pet? " + pet);
-        Console.WriteLine("Me shoe size is " + shoesize); 
-        Console.ReadKey();  
+        Console.WriteLine("Me shoe size is " + shoesize);
+        Console.WriteLine("ByteMin {0} ", byte.MinValue);
+        Console.WriteLine("ByteMax {0}", byte.MaxValue);
+        Console.ReadKey();
     }
-    
-    
-      
 }
