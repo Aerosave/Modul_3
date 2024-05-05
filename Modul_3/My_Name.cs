381fe760e8d4f9c182533e01f89db84382b13135
Nikita Anisimov <nikitanisimov98@gmail.com>
2024-05-05 14:59:23 +1000

    Anketa

--- Modul_3/My_Name.cs
+++ Modul_3/My_Name.cs
@@ -3,16 +3,14 @@ class MainClass
 {
     public static void Main(string[] args)
     {
-        const string MyName = "Никита";
-        byte age = 25;
-        byte shoesize = 44;
-        bool pet = true;
-        Console.WriteLine("My nane is " + MyName);
-        Console.WriteLine("My age is " + age);
-        Console.WriteLine("Do I hane a pet? " + pet);
-        Console.WriteLine("Me shoe size is " + shoesize);
-        Console.WriteLine("ByteMin {0} ", byte.MinValue);
-        Console.WriteLine("ByteMax {0}", byte.MaxValue);
-        Console.ReadKey();
+        Console.WriteLine("What is your name?");
+        var name = Console.ReadLine();
+        Console.WriteLine("What is your age?");
+        var age = checked ((byte)int.Parse(Console.ReadLine()));
+        Console.WriteLine("Your name is {0} and age is {1} ", name, age);
+        Console.WriteLine("What is your birthdate? ");
+        var birthdate = Console.ReadLine();
+        Console.WriteLine("Your birthdate is {0}, your name is {1} and age is {2}", birthdate, name,age);
+
     }
 }
